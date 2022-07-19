07239fc07278618756a4faa480252cc5e269b7b2
LucasPCotrim <lucaspcotrim@gmail.com>
2022-07-19 17:07:39 -0300

    Implemented Layout HomePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import HomePage from './components/HomePage'
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <HomePage />
     </>
   )
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import './css/HomePage.css'
+
+function HomePage() {
+  return (
+    <div className='home-page'>
+      <img src="./img/logo.svg" alt="Logo"/>
+      <h1 className='title'>ZapRecall</h1>
+      <button>Iniciar Recall!</button>
+    </div>
+  )
+}
+
+export default HomePage
